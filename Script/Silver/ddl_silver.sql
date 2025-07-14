0699fbb9f9622ac9da1d9c62ce8a0478470a4e5d
Yashita Yadav <136897381+yashita2305@users.noreply.github.com>
2025-07-14 15:54:25 +0530

    Update ddl_silver.sql

--- Script/Silver/ddl_silver.sql
+++ Script/Silver/ddl_silver.sql
@@ -4,39 +4,51 @@
 -- =========================
 USE silver;
 
--- Sales table: Transactional data
-DROP TABLE IF EXISTS Sales;
-CREATE TABLE Sales (
-    sales_key INT AUTO_INCREMENT PRIMARY KEY,
-    Date DATE,
-    store_id VARCHAR(50),
-    product_id VARCHAR(50),
+CREATE TABLE dim_product (
+    product_id VARCHAR(20) PRIMARY KEY,
+    category VARCHAR(50),
+    
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE dim_store (
+    store_id VARCHAR(20),
+    region VARCHAR(50),
+    store_id_region VARCHAR(100) PRIMARY KEY,
+    
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE dim_date (
+    date DATE PRIMARY KEY,
+    seasonality VARCHAR(20),
+    
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE fact_inventory (
+    date DATE,
+    product_id VARCHAR(20),
+    store_id_region VARCHAR(100),
+
+    inventory_level INT,
     units_sold INT,
     units_ordered INT,
     demand_forecast INT,
-    price FLOAT,
-    discount FLOAT
-);
-
--- Warehouse table: Inventory and contextual data
-DROP TABLE IF EXISTS Warehouse;
-CREATE TABLE Warehouse (
-    warehouse_key INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE,
-    store_id VARCHAR(50),
-    product_id VARCHAR(50),
-    inventory_level INT,
-    weather_condition VARCHAR(20),
+    price DECIMAL(10,2),
+    discount DECIMAL(5,2),
+    competitor_pricing DECIMAL(10,2),
+    weather_condition VARCHAR(50),
     holiday_promotion BOOLEAN,
-    region VARCHAR(50)
-);
 
--- Product table: Product metadata
-DROP TABLE IF EXISTS Product;
-CREATE TABLE Product (
-    product_key INT AUTO_INCREMENT PRIMARY KEY,
-    product_id VARCHAR(50),
-    category VARCHAR(50),
-    competitor_pricing FLOAT,
-    seasonality VARCHAR(20)
+    PRIMARY KEY (date, product_id, store_id_region),
+    FOREIGN KEY (date) REFERENCES dim_date(date),
+    FOREIGN KEY (product_id) REFERENCES dim_product(product_id),
+    FOREIGN KEY (store_id_region) REFERENCES dim_store(store_id_region),
+
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
